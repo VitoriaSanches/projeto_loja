ef0203bac1437730489dc6d7f20a8ab1793be570
Vitória Ferreira Sanches <106158486+VitoriaSanches@users.noreply.github.com>
2023-05-16 11:00:33 -0300

    Add files via upload

--- Projeto Loja/js/script.js
+++ Projeto Loja/js/script.js
@@ -1,5 +1,7 @@
+let modalQt = 1;
+
 const c = (el) => document.querySelector(el);
-const cs = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
 
 //Listagem dos modelos
 modeloJson.map((item, index) => {
@@ -21,20 +23,62 @@ modeloJson.map((item, index) => {
         c('.modeloInfo h1').innerHTML = modeloJson[key].name;
         c('.modeloInfo-desc').innerHTML = modeloJson[key].description;
         c('.modeloInfo-actualPrice').innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`;
+        c('.modeloInfo-size.selected').classList.remove('selected');
 
-        cs('.modeloInfo-size').forEach((size, sizeIndex ) => {
+        cs('.modeloInfo-size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 0){
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML = modeloJson[key].sizes[sizeIndex];
         });
 
+        c('.modeloInfo-qt').innerHTML = modalQt;
         c('.modeloWindowArea').style.opacity = 0;
         c('.modeloWindowArea').style.display = 'flex';
 
-        setTimeout(()=>{
+        setTimeout(() =>{
             c('.modeloWindowArea').style.opacity = 1;
-        }, 200);
+        }, 500);
     });
 
     c('.modelo-area').append(modeloItem);
+});
+
+//eventos Modal
+function closeModal(){
+    c('.modeloWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.modeloWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.modeloInfo-cancelButton, .modeloInfo-cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+//botão qt menos
+c('.modeloInfo-qtmenos').addEventListener('click', () =>{
+    if (modalQt > 1){
+        modalQt--;
+        c('.modeloInfo-qt').innerHTML = modalQt;
+    }
+});
+//botão qt mais
+c('.modeloInfo-qtmais').addEventListener('click', () =>{
+    modalQt++;
+    //atualizo o valor no html
+    c('.modeloInfo-qt').innerHTML = modalQt;
+});
+
+//tamanhos
+cs('.modeloInfo-size').forEach((size, sizeIndex)=>{
+    //ao clicar em um item
+    size.addEventListener('click', () =>{
+        //desmarca tudo
+        c('.modeloInfo-size.selected').classList.remove('selected');
+        //adiciono a marcação
+        size.classList.add('selected');
+    });
 });
 
 
